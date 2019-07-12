b9229e93d54bda5bc70087f414b803959ba7957f
mcattx <mcattx@outlook.com>
2019-07-12 11:18:00 +0800

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+
+import babel from 'rollup-plugin-babel'
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'n3.js',
+    name: 'n3',
+    format: 'umd',
+    banner: `/* @license n3.js v${pkg.version} | (c) mcattx@gmail.com */`,
+    globals: {
+      d3: 'd3'
+    }
+  },
+  plugins: [
+    babel({
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false
+          }
+        ]
+      ]
+    })
+  ],
+  external: ['d3']
+}
